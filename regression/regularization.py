a735165e4f9a790d0b2912d98844669b311aa4ad
tqtifnypmb <tqtifnypmb@gmail.com>
2018-04-24 11:10:54 +0800

    regularization

--- regression/regularization.py
+++ regression/regularization.py
@@ -34,9 +34,15 @@ def predict_and_plot(model, test_x, label):
     pred_y = model.predict(test_x)
     plt.plot(pred_y, label=label)
 
-def draw_figure(num, train_x, train_y, regression_func, title):
+def draw_figure(num, data_x, data_y, regression_func, title):
+    train_x = data_x[30:]
+    train_y = data_y[30:]
+    
+    test_x = data_x[0:30]
+    test_y = data_y[0:30]
+
     plt.figure(num)
-    plt.scatter(train_x[:20], train_y[:20], c='navy', label='training points')
+    plt.scatter(test_x, test_y, c='navy', label='testing points')
 
     for c, degree in enumerate([3, 5, 10, 20]):
         model = regression_func(train_x, train_y, degree)
@@ -49,13 +55,8 @@ def draw_figure(num, train_x, train_y, regression_func, title):
 
 if __name__ == '__main__':
     (data_x, data_y) = generate_data()
-    train_x = data_x[30:]
-    train_y = data_y[30:]
     
-    test_x = data_x[0:30]
-    test_y = data_y[0:30]
-
-    draw_figure(1, train_x, train_y, fit_lasso, 'Lasso')
-    draw_figure(2, train_x, train_y, fit_ridge, 'Ridge')
-    draw_figure(3, train_x, train_y, fit_normal, 'No regularization')
+    draw_figure(1, data_x, data_y, fit_lasso, 'Lasso')
+    draw_figure(2, data_x, data_y, fit_ridge, 'Ridge')
+    draw_figure(3, data_x, data_y, fit_normal, 'No regularization')
     plt.show()
