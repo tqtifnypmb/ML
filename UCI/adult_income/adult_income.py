75c1b5d1f714692be4ac40481031fe5aab170728
tqtifnypmb <tqtifnypmb@gmail.com>
2018-05-17 15:50:58 +0800

    feature selection

--- UCI/adult_income/adult_income.py
+++ UCI/adult_income/adult_income.py
@@ -5,50 +5,34 @@ from sklearn import model_selection
 from sklearn import linear_model
 from sklearn import naive_bayes
 from sklearn import svm
+from sklearn import metrics
 
 COLUMNS_NAME = ['age', 'workclass', 'fnlwgt', 'education', 'education-num', 
-                'marital-status', 'occupation', 'relationship', 'race', 'sex',
+                'marital-status', 'occupation', 'relationship', 'race', 'gender',
                 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country', 'income']
 
 RACE = ['White', 'Asian-Pac-Islander', 'Amer-Indian-Eskimo', 'Other', 'Black']
-MARITAL_STATUS = ['Married-civ-spouse', 'Divorced', 'Never-married', 
-                  'Separated', 'Widowed', 'Married-spouse-absent', 'Married-AF-spouse']
 
 DATA_PATH = ('../uci_data/adult.data.txt',
              '../uci_data/adult.test.txt')
 
 def preprocess(data):
-    # almost bear no information
-    del data['workclass']
-    del data['occupation']
-    del data['native-country']
-    del data['capital-gain']
-    del data['capital-loss']
+    # remove row with missing value
+    data = data[data["workclass"] != "?"]
+    data = data[data["occupation"] != "?"]
+    data = data[data["native-country"] != "?"]
 
-    del data['fnlwgt']
+    data.replace(['Married-civ-spouse', 'Divorced', 'Never-married', 
+                  'Separated', 'Widowed', 'Married-spouse-absent', 'Married-AF-spouse'],
+                 ['married', 'not married', 'not married', 
+                  'not married', 'not married', 'married', 'married'], inplace=True)
 
-    # redundant
-    del data['education']
-    del data['relationship']
+    category_col =['race','marital-status', 'gender', 'income', 'relationship']
+    for col in category_col:
+        _, c = np.unique(data[col], return_inverse=True)
+        data[col] = c
 
-    def convert_str_to_numerical(column, one):
-        origin = data[column]
-        new = origin.str.contains(one)
-        data[column] = new.astype(int)
-
-    # convert sex feature into numerical 1 - Male, 0 - Female
-    convert_str_to_numerical('sex', 'Male')
-
-    # income -> numerical: 1 - <=50k, 0 - >50k
-    convert_str_to_numerical('income', '<=50K')
-
-    def map_list_to_numerical(column, lst):
-        origin = data[column]
-        new = origin.map(lambda x: lst.index(x.strip()))
-        data[column] = new
-
-    map_list_to_numerical('race', RACE)
-    map_list_to_numerical('marital-status', MARITAL_STATUS)
+    return data
 
 def load_data():
     train_data = pd.read_csv(DATA_PATH[0], header=None, names=COLUMNS_NAME)
@@ -57,8 +41,8 @@ def load_data():
     # the first row is invalid
     test_data = test_data.drop([0])
     
-    preprocess(train_data)
-    preprocess(test_data)
+    train_data = preprocess(train_data)
+    test_data = preprocess(test_data)
 
     return (train_data, test_data)
 
@@ -67,35 +51,39 @@ def naive_bayes_model(x_train, y_train, x_test, y_test):
     model.fit(x_train, y_train)
     y_pred = model.predict(x_test)
     
-    miss = (y_pred != y_test).astype(int).sum()
-    print('GaussianNB error rate: %f' % (miss / len(y_test) * 100))
+    acc = metrics.accuracy_score(y_test, y_pred)
+    auc = metrics.roc_auc_score(y_test, y_pred)
+    print('GaussianNB acc: {0}, auc:{1}'.format(acc, auc))
 
 def logistic_regress_model(x_train, y_train, x_test, y_test):
     model = linear_model.LogisticRegressionCV()
     model.fit(x_train, y_train)
     y_pred = model.predict(x_test)
 
-    miss = (y_pred != y_test).astype(int).sum()
-    print('Logistic error rate: %f' % (miss / len(y_test) * 100))
+    acc = metrics.accuracy_score(y_test, y_pred)
+    auc = metrics.roc_auc_score(y_test, y_pred)
+    print('Logistic acc: {0}, auc:{1}'.format(acc, auc))
 
 def svm_model(train_data, test_data):
-    model = svm.SVR()
+    model = svm.SVC()
     model.fit(x_train, y_train)
     y_pred = model.predict(x_test)
 
-    miss = (y_pred != y_test).astype(int).sum()
-    print('SVC error rate: %f' % (miss / len(y_test) * 100))
+    acc = metrics.accuracy_score(y_test, y_pred)
+    auc = metrics.roc_auc_score(y_test, y_pred)
+    print('SVC acc: {0}, auc:{1}'.format(acc, auc))
 
 if __name__ == '__main__':
     train_data, test_data = load_data()
     
-    print(train_data)
-    
+    feature_cols = ['marital-status', 'education-num', 'relationship', 'age']
     y_train = train_data['income']
-    x_train = train_data.drop('income', axis=1)
+    x_train = train_data[feature_cols]
     y_test = test_data['income']
-    x_test = test_data.drop('income', axis=1)
+    x_test = test_data[feature_cols]
 
     naive_bayes_model(x_train, y_train, x_test, y_test)
     logistic_regress_model(x_train, y_train, x_test, y_test)
+
+    #extremely slow
     #svm_model(train_data, test_data)
