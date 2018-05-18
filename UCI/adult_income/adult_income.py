68c0c1ef932d07a0ae083997771c0f689e260765
tqtifnypmb <tqtifnypmb@gmail.com>
2018-05-18 15:18:09 +0800

    metrics

--- UCI/adult_income/adult_income.py
+++ UCI/adult_income/adult_income.py
@@ -6,12 +6,11 @@ from sklearn import linear_model
 from sklearn import naive_bayes
 from sklearn import svm
 from sklearn import metrics
+from sklearn import feature_selection
 
 COLUMNS_NAME = ['age', 'workclass', 'fnlwgt', 'education', 'education-num', 
                 'marital-status', 'occupation', 'relationship', 'race', 'gender',
                 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country', 'income']
-
-RACE = ['White', 'Asian-Pac-Islander', 'Amer-Indian-Eskimo', 'Other', 'Black']
 
 DATA_PATH = ('../uci_data/adult.data.txt',
              '../uci_data/adult.test.txt')
@@ -27,7 +26,8 @@ def preprocess(data):
                  ['married', 'not married', 'not married', 
                   'not married', 'not married', 'married', 'married'], inplace=True)
 
-    category_col =['race','marital-status', 'gender', 'income', 'relationship']
+    category_col =['race','marital-status', 'gender', 'income', 'relationship', 
+                   'native-country', 'workclass', 'occupation', 'education']
     for col in category_col:
         _, c = np.unique(data[col], return_inverse=True)
         data[col] = c
@@ -46,32 +46,32 @@ def load_data():
 
     return (train_data, test_data)
 
+def cal_scores(y_pred, y_true, name):
+    acc = metrics.accuracy_score(y_test, y_pred)
+    auc = metrics.roc_auc_score(y_test, y_pred)
+    logloss = metrics.log_loss(y_test, y_pred)
+    print('{name} \nacc: {0} \nauc:{1} \nlogloss:{2}\n========='.format(acc, auc, logloss, name=name))
+
 def naive_bayes_model(x_train, y_train, x_test, y_test):
     model = naive_bayes.GaussianNB()
     model.fit(x_train, y_train)
     y_pred = model.predict(x_test)
     
-    acc = metrics.accuracy_score(y_test, y_pred)
-    auc = metrics.roc_auc_score(y_test, y_pred)
-    print('GaussianNB acc: {0}, auc:{1}'.format(acc, auc))
+    cal_scores(y_pred, y_test, 'GaussianNB')
 
 def logistic_regress_model(x_train, y_train, x_test, y_test):
     model = linear_model.LogisticRegressionCV()
     model.fit(x_train, y_train)
     y_pred = model.predict(x_test)
 
-    acc = metrics.accuracy_score(y_test, y_pred)
-    auc = metrics.roc_auc_score(y_test, y_pred)
-    print('Logistic acc: {0}, auc:{1}'.format(acc, auc))
+    cal_scores(y_pred, y_test, 'Logistic')
 
 def svm_model(train_data, test_data):
     model = svm.SVC()
     model.fit(x_train, y_train)
     y_pred = model.predict(x_test)
 
-    acc = metrics.accuracy_score(y_test, y_pred)
-    auc = metrics.roc_auc_score(y_test, y_pred)
-    print('SVC acc: {0}, auc:{1}'.format(acc, auc))
+    cal_scores(y_pred, y_test, 'SVC')
 
 if __name__ == '__main__':
     train_data, test_data = load_data()
