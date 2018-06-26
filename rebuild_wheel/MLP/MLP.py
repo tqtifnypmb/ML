0089c9e40c7dcef96e4eeb831281820d53a957ad
tqtifnypmb <tqtifnypmb@gmail.com>
2018-06-26 19:32:07 +0800

    backward

--- rebuild_wheel/MLP/MLP.py
+++ rebuild_wheel/MLP/MLP.py
@@ -1,11 +1,14 @@
 import numpy as np
 import time
+
 from math import exp
+from sklearn import metrics
 
 class MLP:
-    def __init__(self, hidden_layer_sizes):
+    def __init__(self, hidden_layer_sizes, learning_rate = 0.001):
         self.hidden_layer_sizes_ = hidden_layer_sizes
         self.rd = np.random.RandomState(seed=123456)
+        self.learning_rate_ = learning_rate
 
     def _relu(self, x):
         return max(0, x)
@@ -13,20 +16,22 @@ class MLP:
     def _sigmoid(self, x):
         return exp(x) / (1 + exp(x))
 
-    def _forward(self, features, label):
+    def _forward(self, features):
         n_units = self.hidden_layer_sizes_[0]
         
         for i in range(n_units):
             w = self.weights[i]
-            self.hidden_values[i] = np.matmul(features, w)
+            self.hidden_values[i] =  self._relu(np.matmul(features, w))
 
-        sv = np.apply_along_axis(self._relu, 1, self.hidden_values)
-        output_value = np.matmul(np.transpose(self.output_weights), sv)
-        
+        output_value = np.matmul(np.transpose(self.output_weights), self.hidden_values)
+
         return self._sigmoid(output_value[0][0])
 
-    def _backward(self):
-        pass
+    def _backward(self, pred, label):
+        n_units = self.hidden_layer_sizes_[0]
+        phis = np.zeros((n_units, 1))
+        phi_0 = label - pred
+        
 
     def _is_convergent(self, output, y):
         return False
@@ -38,12 +43,12 @@ class MLP:
             features = self.samples[epoch]
             label = self.labels[epoch]
 
-            output = self._forward(features, label)
+            pred = self._forward(features)
             
-            if self._is_convergent(output, label):
+            if self._is_convergent(pred, label):
                 break
 
-            self._backward()
+            self._backward(pred, label)
 
     def fit(self, x, y):
         if x.shape[0] != y.shape[0]:
@@ -67,9 +72,23 @@ class MLP:
     def predict(self, x):
         pass
 
+
+def cal_metrics(y_true, y_pred):
+    acc = metrics.accuracy_score(y_true, y_pred)
+    print("acc {0}".format(acc))
+
+    prec = metrics.precision_score(y_true, y_pred, average='weighted')
+    print("precision {0}".format(prec))
+
+    recall = metrics.recall_score(y_true, y_pred, average='weighted')
+    print("recall {0}".format(recall))
+
 if __name__ == '__main__':
     mlp = MLP((3,1))
 
     x = np.arange(0, 100).reshape((10, -1))
     y = np.arange(0,10).reshape(10, -1)
-    y_pred = mlp.fit(x, y)
+    mlp.fit(x, y)
+
+    y_pred = mlp.predict(x)
+    # cal_metrics(y, y_pred)
