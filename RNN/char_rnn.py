985af144fcaa8e4e6b83badc196be5c2124d3993
tqtifnypmb <tqtifnypmb@gmail.com>
2018-08-07 08:16:10 +0800

    rnn

--- RNN/char_rnn.py
+++ RNN/char_rnn.py
@@ -2,28 +2,32 @@ import tensorflow as tf
 import numpy as np
 
 class RNN: 
-    def __init__(self, hidden_size, vocab_size, seq_len, batch_size):
-        self.hidden_size_ = hidden_size
-        self.vocab_size_ = vocab_size
-        self.seq_len_ = seq_len
-        self.batch_size_ = batch_size
+    def __init__(self, num_classes, hidden_size, num_layers, dropout_prob, vocab_size, seq_len, batch_size):
+        cell, self.initial_state = self._build_lstm(hidden_size, num_layers, batch_size, dropout_prob)
+        self.inputs, self.targets = self._build_inputs(batch_size, seq_len)
 
-        self.wxh_ = tf.Variable(tf.random_normal((batch_size, vocab_size)))
-        self.whh_ = tf.Variable(tf.random_normal((hidden_size, hidden_size)))
-        self.why_ = tf.Variable(tf.random_normal((hidden_size, vocab_size)))
-        x = tf.placeholder(tf.float32, shape=(batch_size, vocab_size), name='X')
+        outputs, self.final_state = tf.nn.dynamic_rnn(cell, self.inputs, initial_state=self.initial_state)
 
-        self.h = np.zeros((hidden_size, hidden_size))
-        xh = tf.matmul(x, self.wxh_, transpose_b=True)
-        hh = tf.matmul(self.h, self.whh_, transpose_b=True)
-        self.h = tf.nn.tanh(xh + hh)
-        y = tf.matmul(self.h, self.why_, transpose_b=True)
+        self.loss = self._build_loss(outputs, self.targets, num_classes)
+     
+    def _build_inputs(self, batch_size, seq_len):
+        inputs = tf.placeholder(tf.float32, (batch_size, seq_len), 'inputs')
+        targets = tf.placeholder(tf.float32, (batch_size, seq_len), 'targets')
+        return inputs, targets
 
-    def train(self, x):
-        pass
+    def _build_lstm(self, hidden_size, num_layers, batch_size, dropout_prob):
+        lstm = tf.nn.rnn_cell.BasicLSTMCell(hidden_size)
+        lstm_cell = tf.nn.rnn_cell.DropoutWrapper(lstm, output_keep_prob=dropout_prob)
+        cells = tf.nn.rnn_cell.MultiRNNCell([lstm_cell for _ in range(num_layers)])
+        initial_state = cells.zero_state(batch_size, tf.float32)
+        return cells, initial_state
 
-    def predict(self, x):
-        pass
+    def _build_loss(self, logits, targets, num_classes):
+        encoded_targets = tf.one_hot(targets, num_classes)
+        reshaped_targets = tf.reshape(encoded_targets, logits.shape)
+        
+        loss = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=reshaped_targets)
+        return tf.reduce_mean(loss)
 
 if __name__ == '__main__':
     pass
