b7a7575996f877c38b57eb7f839c75e1e1d629da
tqtifnypmb <tqtifnypmb@gmail.com>
2018-08-10 17:31:12 +0800

    invalid dimensions

--- RNN/char_rnn.py
+++ RNN/char_rnn.py
@@ -1,40 +1,43 @@
 import tensorflow as tf
+import numpy as np
 
 class RNN: 
-    def __init__(self, num_classes, hidden_size, num_layers, dropout_prob, seq_len, batch_size):
-        cell, self.initial_state = self._build_lstm(hidden_size, num_layers, batch_size, dropout_prob)
-        self.inputs, self.targets = self._build_inputs(batch_size, seq_len)
-        outputs, self.final_state = tf.nn.dynamic_rnn(cell, tf.expand_dims(self.inputs, -1), initial_state=self.initial_state)
-        logits = self._build_output(outputs, seq_len, batch_size, num_classes)
+    def __init__(self, vocab_size, unit_per_cell, num_of_layers, seq_len, batch_size):
+        self.inputs, self.targets = self._build_inputs(seq_len)
+        self.cell = self._build_lstm_cell(unit_per_cell, num_of_layers)
+        self.initial_state = self.cell.zero_state(batch_size, tf.float32)
 
-        self.loss = self._build_loss(logits, self.targets, num_classes)
-     
-    def _build_inputs(self, batch_size, seq_len):
-        inputs = tf.placeholder(tf.float32, (batch_size, seq_len), 'inputs')
-        targets = tf.placeholder(tf.float32, (batch_size, seq_len), 'targets')
-        return inputs, targets
+        x = tf.reshape(self.inputs, [-1, seq_len])
+        x = tf.split(x, seq_len, 1)
+        y, state = tf.nn.static_rnn(self.cell, x, dtype=tf.float32)
+        self.final_state = state
 
-    def _build_lstm(self, hidden_size, num_layers, batch_size, dropout_prob):
-        lstm = tf.nn.rnn_cell.BasicLSTMCell(hidden_size)
-        lstm_cell = tf.nn.rnn_cell.DropoutWrapper(lstm, output_keep_prob=dropout_prob)
-        cells = lstm_cell
-        # cells = tf.nn.rnn_cell.MultiRNNCell([lstm_cell for _ in range(num_layers)])
-        initial_state = cells.zero_state(batch_size, tf.float32)
-        return cells, initial_state
+        # self.loss = None
+        self.weights = tf.random_normal([unit_per_cell, vocab_size])
+        self.bias = tf.Variable(tf.random_normal([vocab_size]))
+        output = tf.matmul(y[-1], self.weights) + self.bias
 
-    def _build_loss(self, logits, targets, num_classes):
-        encoded_targets = tf.one_hot(targets, num_classes)
-        reshaped_targets = tf.reshape(encoded_targets, logits.shape)
+        encoded_targets = tf.one_hot(tf.cast(self.targets, tf.int32), vocab_size)
+        encoded_targets = tf.cast(encoded_targets, tf.float32)
+        self.loss = self._build_loss(output, encoded_targets)
         
-        loss = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=reshaped_targets)
-        return tf.reduce_mean(loss)
+    def _build_inputs(self, seq_len):
+        x = tf.placeholder(tf.float32, [None, seq_len], name="inputs")
+        y = tf.placeholder(tf.float32, [None, seq_len], name="targets")
+        return x, y
 
-    def _build_output(self, output, seq_len, batch_size, num_classes):
-        with tf.variable_scope('softmax'):
-            w = tf.get_variable('w', (batch_size, seq_len))
-            b = tf.get_variable('b', (num_classes,), initializer=tf.constant_initializer(0.0))
-        logits = tf.matmul(output, w) + b
-        return logits
+    def _build_lstm_cell(self, unit_per_cell, num_of_layers):
+        cell = tf.nn.rnn_cell.BasicLSTMCell(unit_per_cell)
+        if num_of_layers == 1:
+            return cell
+        else:
+            cells = tf.nn.rnn_cell.MultiRNNCell([cell for _ in range(num_of_layers)])
+            return cells
+
+    def _build_loss(self, y_pred, y_true):
+        loss = tf.losses.softmax_cross_entropy(y_true, y_pred)
+        cost = tf.reduce_mean(loss)
+        return cost
 
 def batch_sample_generator(sample, batch_size):
     batch_num = int(len(sample) / batch_size)
@@ -45,7 +48,7 @@ def batch_sample_generator(sample, batch_size):
             y[-1] = sample[(batch_idx + 1) * batch_size + 1]
         else:
             y[-1] = 0
-        yield x, y
+        yield np.asarray(x), np.asarray(y)
 
 def preprocess_sample(sample):
     duplicated = set()
@@ -64,22 +67,21 @@ if __name__ == '__main__':
 
     vocab_index, index_dict = preprocess_sample(vocab)
     batch_size = 20
-    model = RNN(len(index_dict), 4, 4, 0.5, 1, batch_size)
+    seq_len = 1
+    model = RNN(len(index_dict), 256, 1, seq_len, batch_size)
     
+    tf.global_variables_initializer()
     with tf.Session() as sess:
-        sess.initialize_all_variables()
-
         newest_state = sess.run(model.initial_state)
-        optimizer = tf.train.AdamOptimizer()
-        optimier_aciton = optimizer.minimize(model.loss)
+        optimizer = tf.train.AdamOptimizer().minimize(model.loss)
         for batch_x, batch_y in batch_sample_generator(vocab_index, batch_size):
             feed = {
-                model.inputs: batch_x.reshpae(model.inputs.shape),
-                model.targets: batch_y.reshape(model.targets.shape),
+                model.inputs: batch_x.reshape(-1, seq_len),
+                model.targets: batch_y,
                 model.initial_state: newest_state,
             }
 
-            batch_loss, _ = sess.run([model.loss, optimier_aciton], feed_dict=feed)
+            batch_loss, _ = sess.run([model.loss, optimizer], feed_dict=feed)
             newest_state = sess.run(model.final_state)
 
             print('batch loss %d' % batch_loss)
