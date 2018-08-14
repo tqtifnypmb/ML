0fd5ab43f80d303146c50585225a4061ed32b890
tqtifnypmb <tqtifnypmb@gmail.com>
2018-08-14 12:25:17 +0800

    what?

--- RNN/char_rnn.py
+++ RNN/char_rnn.py
@@ -7,21 +7,22 @@ class RNN:
         self.cell = self._build_lstm_cell(num_units, num_layers)
         self.initial_state = self.cell.zero_state(batch_size, tf.float32)
 
+        x = tf.stack([self.inputs], 1)
         x = tf.reshape(self.inputs, [-1, seq_len])
         x = tf.split(x, seq_len, 1)
         y, state = tf.nn.static_rnn(self.cell, x, dtype=tf.float32)
         self.final_state = state
 
-        # self.loss = None
         self.weights = tf.random_normal([num_units, num_classes])
         self.bias = tf.Variable(tf.random_normal([num_classes]))
         output = tf.matmul(y[-1], self.weights) + self.bias
 
-        self.loss = self._build_loss(output, self.targets)
+        encoded_targets = tf.one_hot(self.targets, num_classes, axis=1)
+        self.loss = self._build_loss(output, encoded_targets)
         
     def _build_inputs(self, seq_len):
-        x = tf.placeholder(tf.float32, [None, seq_len, 1], name="inputs")
-        y = tf.placeholder(tf.float32, [None, seq_len], name="targets")
+        x = tf.placeholder(tf.float32, [None, seq_len], name="inputs")
+        y = tf.placeholder(tf.int32, [None, 1], name="targets")
         return x, y
 
     def _build_lstm_cell(self, unit_per_cell, num_of_layers):
@@ -33,7 +34,7 @@ class RNN:
             return cells
 
     def _build_loss(self, y_pred, y_true):
-        loss = tf.losses.softmax_cross_entropy(y_true, y_pred)
+        loss = tf.nn.softmax_cross_entropy_with_logits_v2(labels=y_true, logits=y_pred)
         cost = tf.reduce_mean(loss)
         return cost
 
@@ -41,11 +42,12 @@ def batch_sample_generator(sample, batch_size):
     batch_num = int(len(sample) / batch_size)
     for batch_idx in range(batch_num):
         x = sample[batch_idx * batch_size : (batch_idx + 1) * batch_size]
-        y = x[1:] + [x[0]]
+
+        y = None
         if (batch_idx + 1) * batch_size < len(sample):
-            y[-1] = sample[(batch_idx + 1) * batch_size + 1]
+            y = [sample[(batch_idx + 1) * batch_size + 1]]
         else:
-            y[-1] = 0
+            y = [sample[0]]
         yield np.asarray(x), np.asarray(y)
 
 def preprocess_sample(sample):
@@ -68,19 +70,20 @@ if __name__ == '__main__':
     num_classes = len(index_dict)
     seq_len = 20
     num_units = 20
-    num_layers = num_classes
+    num_layers = 1
     model = RNN(num_classes, num_units, num_layers, seq_len, batch_size)
     
-    tf.global_variables_initializer()
     with tf.Session() as sess:
         newest_state = sess.run(model.initial_state)
         optimizer = tf.train.AdamOptimizer().minimize(model.loss)
-        for batch_x, batch_y in batch_sample_generator(vocab_index, batch_size):
+        sess.run(tf.global_variables_initializer())
+        for batch_x, batch_y in batch_sample_generator(vocab_index, batch_size * seq_len):
             x = batch_x.reshape(-1, seq_len)
-            x = np.split(x, seq_len, 1)
+            y = batch_y.reshape(-1, 1)
+            # x = np.split(x, seq_len, 1)
             feed = {
                 model.inputs: x,
-                model.targets: batch_y,
+                model.targets: y,
                 model.initial_state: newest_state,
             }
 
