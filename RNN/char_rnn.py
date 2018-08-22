89633bf9a91092503f519be83453006f60b60e7b
tqtifnypmb <tqtifnypmb@gmail.com>
2018-08-22 20:39:38 +0800

    char rnn

--- RNN/char_rnn.py
+++ RNN/char_rnn.py
@@ -2,47 +2,31 @@ import tensorflow as tf
 import numpy as np
 
 class RNN: 
-    def __init__(self, num_classes, num_units, num_layers, seq_len, batch_size):
-        self.inputs, self.targets = self._build_inputs(seq_len)
-        self.cell = self._build_lstm_cell(num_units, num_layers)
-        self.initial_state = self.cell.zero_state(batch_size, tf.float32)
+    def __init__(self, seq_len, num_classes, num_units, batch_size):
+        self.inputs = tf.placeholder(tf.float32, [None, 1, seq_len], name="inputs")
+        self.targets = tf.placeholder(tf.float32, [None, num_classes])
+        self.cell = tf.nn.rnn_cell.BasicLSTMCell(num_units)
 
-        x = tf.stack([self.inputs], 1)
-        x = tf.reshape(self.inputs, [-1, seq_len])
-        x = tf.split(x, seq_len, 1)
-        y, state = tf.nn.static_rnn(self.cell, x, dtype=tf.float32)
-        self.final_state = state
+        x = tf.unstack(self.inputs, 1, axis=1)
+        outputs, _ = tf.nn.static_rnn(self.cell, x, dtype=tf.float32)
 
-        self.weights = tf.random_normal([num_units, num_classes])
+        self.weight = tf.Variable(tf.random_normal([num_units, num_classes]))
         self.bias = tf.Variable(tf.random_normal([num_classes]))
-        output = tf.matmul(y[-1], self.weights) + self.bias
 
-        encoded_targets = tf.one_hot(self.targets, num_classes, axis=1)
-        self.loss = self._build_loss(output, encoded_targets)
+        self.logits = tf.matmul(outputs[-1], self.weight) + self.bias
+
+        self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.logits, 
+                                                                           labels=self.targets))
         
-    def _build_inputs(self, seq_len):
-        x = tf.placeholder(tf.float32, [None, seq_len], name="inputs")
-        y = tf.placeholder(tf.int32, [None, 1], name="targets")
-        return x, y
-
-    def _build_lstm_cell(self, unit_per_cell, num_of_layers):
-        cell = tf.nn.rnn_cell.BasicLSTMCell(unit_per_cell)
-        if num_of_layers == 1:
-            return cell
-        else:
-            cells = tf.nn.rnn_cell.MultiRNNCell([cell for _ in range(num_of_layers)])
-            return cells
-
-    def _build_loss(self, y_pred, y_true):
-        loss = tf.nn.softmax_cross_entropy_with_logits_v2(labels=y_true, logits=y_pred)
-        cost = tf.reduce_mean(loss)
-        return cost
+        pred = tf.nn.softmax(self.logits)
+        pred = tf.reduce_mean(pred, 0)
+        pred = tf.reshape(pred, (1,-1))
+        self.prediction = tf.argmax(pred, 1)
 
 def batch_sample_generator(sample, batch_size):
     batch_num = int(len(sample) / batch_size)
     for batch_idx in range(batch_num):
         x = sample[batch_idx * batch_size : (batch_idx + 1) * batch_size]
-
         y = None
         if (batch_idx + 1) * batch_size < len(sample):
             y = [sample[(batch_idx + 1) * batch_size + 1]]
@@ -68,26 +52,28 @@ if __name__ == '__main__':
     vocab_index, index_dict = preprocess_sample(vocab)
     batch_size = 60
     num_classes = len(index_dict)
-    seq_len = 20
+    seq_len = 3
     num_units = 20
-    num_layers = 1
-    model = RNN(num_classes, num_units, num_layers, seq_len, batch_size)
-    
+    model = RNN(seq_len, num_classes, num_units, batch_size)
+
     with tf.Session() as sess:
-        newest_state = sess.run(model.initial_state)
         optimizer = tf.train.AdamOptimizer().minimize(model.loss)
         sess.run(tf.global_variables_initializer())
+
         for batch_x, batch_y in batch_sample_generator(vocab_index, batch_size * seq_len):
-            x = batch_x.reshape(-1, seq_len)
-            y = batch_y.reshape(-1, 1)
-            # x = np.split(x, seq_len, 1)
+            x = batch_x.reshape(-1, 1, seq_len)
+            y_one_hot = np.zeros([1, num_classes])
+            y_one_hot[0][batch_y] = 1
             feed = {
                 model.inputs: x,
-                model.targets: y,
-                model.initial_state: newest_state,
+                model.targets: y_one_hot,
             }
 
             batch_loss, _ = sess.run([model.loss, optimizer], feed_dict=feed)
-            newest_state = sess.run(model.final_state)
-
+            pred = sess.run([model.prediction], feed_dict=feed)
+            pred_idx = pred[0][0]
             print('batch loss %d' % batch_loss)
+            print("{0} {1} {2} - {3}".format(index_dict[batch_x[0]],
+                                             index_dict[batch_x[1]],
+                                             index_dict[batch_x[2]],
+                                             index_dict[pred_idx]))
