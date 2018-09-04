1f8def2a78344c36aa3d20bf4aa67756ed5e6a6c
tqtifnypmb <tqtifnypmb@gmail.com>
2018-09-04 08:52:08 +0800

    use placeholder to store lstm state instead

--- RNN/LSTM/trainer/char_lstm.py
+++ RNN/LSTM/trainer/char_lstm.py
@@ -8,6 +8,7 @@ from tensorflow.python.lib.io import file_io
 
 class LSTM:
     def __init__(self, num_units, num_layers, seq_len, batch_size, vocab_size):
+        self.batch_size = batch_size
         self.inputs = tf.placeholder(tf.float32, [None, seq_len, vocab_size], name='inputs')
         self.targets = tf.placeholder(tf.float32, [None, seq_len, vocab_size], name='targets')
         labels = tf.unstack(self.targets, seq_len, 1)
@@ -17,23 +18,24 @@ class LSTM:
         else:
             self.cell = self._build_cell(num_units)
 
-        initial_states = self._build_state_variables(self.cell, batch_size)
+        self.initial_states = self.cell.zero_state(batch_size, tf.float32)
 
-        hidden_state, final_state = tf.nn.dynamic_rnn(self.cell, self.inputs, initial_state=initial_states)
+        # initial_states = self._build_state_variables(self.cell, batch_size)
 
+        hidden_state, self.final_state = tf.nn.dynamic_rnn(self.cell, self.inputs, initial_state=self.initial_states)
         hidden_state = tf.unstack(hidden_state, seq_len, 1)
        
         self.weights = tf.Variable(tf.random_normal([num_units, vocab_size]))
         self.bias = tf.Variable(tf.random_normal([vocab_size]))
 
-        Y = tf.matmul(hidden_state[-1], self.weights) + self.bias
-        batch_loss = tf.nn.softmax_cross_entropy_with_logits_v2(logits=Y, labels=labels)
+        self.output = tf.matmul(hidden_state[-1], self.weights) + self.bias
+        batch_loss = tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.output, labels=labels)
         self.loss = tf.reduce_mean(batch_loss)
 
-        self.update_state = self._state_variables_update_op(initial_states, final_state)
+        # self.update_state = self._state_variables_update_op(initial_states, final_state)
 
-        zero_state = self._build_state_variables(self.cell, batch_size)
-        self.reset_state = self._state_variables_update_op(initial_states, zero_state)
+        # zero_state = self._build_state_variables(self.cell, batch_size)
+        # self.reset_state = self._state_variables_update_op(initial_states, zero_state)
 
     def _build_cell(self, num_units):
         cell = tf.nn.rnn_cell.BasicLSTMCell(num_units)
@@ -71,36 +73,28 @@ class LSTM:
         return update_ops
 
     def predict(self, sess, init_value, output_len, seq_len, idx_to_char):
-        vocab_len = len(idx_to_char)
-
-        cur_state = self.cell.zero_state(1, tf.float32)
-
-        value = tf.placeholder(tf.float32, [None, vocab_len], name='pred_value')
+        cur_state = sess.run(self.cell.zero_state(self.batch_size, tf.float32))
       
-        cur_value = np.zeros([vocab_len])
-        cur_value[init_value] = 1
-        
+        prev = init_value
         pred = []
         for i in range(output_len):
             print('generating character: %i ' % i)
-
-            outputs, state = self.cell(inputs=value, state=cur_state)
-            cur_state = state
-            
-            Y = tf.matmul(outputs, self.weights) + self.bias
-            
+        
             feed_dict = {
-                self.inputs: np.zeros([1, seq_len, vocab_len]),
-                self.targets: np.zeros([1, seq_len, vocab_len]),
-                value: cur_value.reshape(-1, vocab_len)
+                self.inputs: prev,
+                self.initial_states: cur_state,
             }
 
-            py = sess.run([Y], feed_dict=feed_dict)
-            idx = np.argmax(py[-1])
-     
-            cur_value = np.zeros_like(cur_value)
-            cur_value[idx] = 1
+            py, state = sess.run([self.output, self.final_state], feed_dict=feed_dict)
+            cur_state = state
 
+            idx = np.argmax(py[-1])
+            prev[0: len(prev) - 1] = prev[1: len(prev)]
+
+            value = np.zeros_like(prev[-1][0])
+            value[idx] = 1
+            prev[-1] = value
+           
             char = idx_to_char[idx]
             pred.append(char)
 
@@ -172,42 +166,40 @@ def main(input_file_name,
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
 
-        dump_inputs = np.zeros([1, seq_len, vocab_len])
-        dump_targets = np.zeros([1, seq_len, vocab_len])
-
         print('training...')
+        pred_inputs, _ = next(next_batch(sample, batch_size, seq_len, char_to_idx))
         for i in range(num_epoches):
             print('===============')
             print('epoch: %d' % i)
             print('===============')
+
+            cur_state = sess.run(model.initial_states)
             for inputs, targets in next_batch(sample, batch_size, seq_len, char_to_idx):
                 feed_dict = {
                     model.inputs: inputs,
-                    model.targets: targets
+                    model.targets: targets,
+                    model.initial_states: cur_state
                 }
 
-                _, _, loss = sess.run([optimizer, model.update_state, model.loss], feed_dict=feed_dict)
+                _, state, loss = sess.run([optimizer, model.final_state, model.loss], feed_dict=feed_dict)
+                cur_state = state
                 print(loss)
 
             if i != 0 and i % check_point == 0:
                 output.write('[epoch %d] predicting...\n' % i)
-                init_char = 'b'
+                
                 with tf.device(device_name):
-                    pred = model.predict(sess, char_to_idx[init_char], output_len, seq_len, idx_to_char)
+                    pred = model.predict(sess, pred_inputs, output_len, seq_len, idx_to_char)
                 output.write("".join(pred))
                 output.write("\n\n")
                 print("".join(pred))
 
             # reset state
-            sess.run([model.reset_state], feed_dict= {
-                model.inputs: dump_inputs,
-                model.targets: dump_targets
-            })
+            # sess.run([model.reset_state])
 
         print('predicting...\n')
-        init_char = 'b'
         with tf.device(device_name):
-            pred = model.predict(sess, char_to_idx[init_char], output_len, seq_len, idx_to_char)
+            pred = model.predict(sess, pred_inputs, output_len, seq_len, idx_to_char)
         output.write("".join(pred))
         output.write("\n\n")
         print("".join(pred))
