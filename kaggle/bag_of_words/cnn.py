f07ecd04a2f7503b4fc64081f7852cb3abd06735
tqtifnypmb <tqtifnypmb@gmail.com>
2018-09-20 14:33:49 +0800

    cnn

--- kaggle/bag_of_words/cnn.py
+++ kaggle/bag_of_words/cnn.py
@@ -2,30 +2,35 @@ import pandas as pd
 import re
 import nltk
 import os
+import numpy as np
 
 from gensim.models import word2vec, KeyedVectors
 from sklearn import model_selection
 from tensorflow import keras
 from bs4 import BeautifulSoup
 
-def build_mode(vocab_size, num_features, num_conv, kernel_size, num_dense, num_units):
+def build_mode(vocab_size, num_words, num_features, num_conv, kernel_size, num_dense, num_units):
     model = keras.Sequential()
 
     # input
-    input = keras.layers.InputLayer(input_shape=[1, num_features])
+    input = keras.layers.InputLayer(input_shape=[num_words, num_features], name='input')
     model.add(input)
-
-    # embed = keras.layers.Embedding(vocab_size, vocab_size, input_length=1)
-    # model.add(embed)
 
     for _ in range(num_conv):
         # convoluntion
-        conv = keras.layers.Convolution1D(1, kernel_size, activation='relu')
+        conv = keras.layers.Conv1D(1, kernel_size, activation='relu', padding='same')
         model.add(conv)
 
         # dense
-        pool = keras.layers.MaxPool1D()
+        pool = keras.layers.MaxPool1D(padding='same')
         model.add(pool)
+
+        # dropout
+        dropout = keras.layers.Dropout(0.25)
+        model.add(dropout)
+
+    # flatten
+    model.add(keras.layers.Flatten())
 
     for _ in range(num_dense):
         # dense layers
@@ -56,18 +61,16 @@ def review_to_words(raw_review, remove_stop_words = False):
     else:
         return words
 
-def review_to_sentences(raw_review, tokenizer, remove_stop_words = False):
+def review_to_sentence(raw_review, tokenizer, remove_stop_words = False):
     raw_review = raw_review.decode('utf-8')
     raw_sentences = tokenizer.tokenize(raw_review.strip())
-    sentences = []
-    total_words = []
+    sentence = []
     for raw_sentence in raw_sentences:
         if len(raw_sentence) > 0:
             words = review_to_words(raw_sentence, remove_stop_words)
-            sentences.append(words)
-            total_words += words
+            sentence += words
 
-    return sentences, total_words
+    return sentence
 
 def read_data(file_name):
     data = pd.read_csv(file_name,
@@ -86,55 +89,79 @@ def encode_reviews(raw_reviews, char_to_idx):
     return ret
 
 def encode_reviews_2(raw_reviews, tokenizer):
-    ret_sen = []
-    ret_words = []
+    sentences = []
     for i in range(raw_reviews.size):
         print('encoding review: %.1f%%\r' % (float(i) / raw_reviews.size * 100))
         text = raw_reviews[i]
-        sentences, words = review_to_sentences(text, tokenizer)
-        ret_sen += sentences
-        ret_words += words
-    return ret_sen, ret_words
+        sentence = review_to_sentence(text, tokenizer)
+        sentences.append(sentence)
+    return sentences
 
-def train_and_save_word2vec(raw_reviews, num_features, output_file):
-    tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-    sentences, words = encode_reviews_2(train['review'], tokenizer)
-    model = word2vec.Word2Vec(size=num_features)
-    model.build_vocab(words, update=False)
-    model.train(sentences, total_examples=len(sentences), epochs=1)
+def train_and_save_word2vec(raw_reviews, num_features, tokenizer, output_file):
+    sentences = encode_reviews_2(raw_reviews, tokenizer)
+    model = word2vec.Word2Vec(sentences, size=num_features, min_count=1)
     model.init_sims(replace=True)
     model.wv.save_word2vec_format(output_file)
-    return model
+    return model, sentences
+
+def vector_for_sentence(sent, num_features, model):
+    vec = np.zeros([num_features])
+    for word in sent:
+        v = model[word]
+        vec += v
+
+    vec /= len(sent)
+    return np.reshape(np.asarray(vec), [1, -1])
 
 if __name__ == '__main__':
-    train = read_data('data/labeledTrainData_truncated.tsv')
+    train = read_data('data/labeledTrainData.tsv')
+    tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 
     wordModel = None
-    num_features = 500
+    num_features = 30
     word2vec_model_file = 'word2vec_model.model'
+    sents = None
     if os.path.isfile(word2vec_model_file):
+        sents = encode_reviews_2(train['review'], tokenizer)
         wordModel = KeyedVectors.load_word2vec_format(word2vec_model_file)
     else:
-        wordModel = train_and_save_word2vec(train['review'], 
-                                            num_features, 
-                                            word2vec_model_file)
+        wordModel, sents = train_and_save_word2vec(train['review'],
+                                                   num_features,
+                                                   tokenizer,
+                                                   word2vec_model_file)
 
     idx_to_word = wordModel.wv.index2word
 
     batch_size = 32
     vocab_size = len(idx_to_word)
-    num_conv = 6
+    num_conv = 1
     kernel_size = 3
     num_dense = 3
     num_units = 128
-    model = build_mode(vocab_size,num_features,  num_conv, kernel_size, num_dense, num_units)
+    num_words = 1
+    model = build_mode(vocab_size, num_words, num_features, num_conv, kernel_size, num_dense, num_units)
 
-    # train_x, test_x, train_y, test_y = model_selection.train_test_split(train['review'], 
-    #                                                                     train['sentiment'], 
-    #                                                                     test_size=0.3)
+    X = np.zeros([len(sents), 1, num_features])
+    for i in range(len(sents)):
+        sent = sents[i]
+        vec = vector_for_sentence(sent, num_features, wordModel)
+        X[i][0] = vec
     
-    x = wordModel.wv.syn0 #train['review']
     y = train['sentiment']
-    model.fit(x, y, batch_size)
-    # pred = model.predict(test_x, batch_size=batch_size)
-    # print(pred)
+    model.fit(X, y, batch_size)
+
+    # test
+    test = read_data('data/testData.tsv')
+    test_sents = encode_reviews_2(test['review'], tokenizer)
+    wordModel.build_vocab(test_sents, update=True)
+    test_x = np.zeros([len(test_sents), 1, num_features])
+    for i in range(len(test_sents)):
+        sent = test_sents[i]
+        vec = vector_for_sentence(sent, num_features, wordModel)
+        test_x[i][0] = vec
+
+    pred = model.predict(test_x)
+    pred = np.squeeze(pred)
+    print(pred)
+    output = pd.DataFrame( data={"id":test["id"], "sentiment":pred} )
+    output.to_csv( "Bag_of_Words_model.csv", index=False, quoting=3 )
