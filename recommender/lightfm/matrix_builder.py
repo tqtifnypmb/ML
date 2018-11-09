6c80874b0f67c6218a298886915fea048cb14336
tqtifnypmb <tqtifnypmb@gmail.com>
2018-11-09 12:28:00 +0800

    rec

--- recommender/lightfm/matrix_builder.py
+++ recommender/lightfm/matrix_builder.py
@@ -8,87 +8,69 @@ import datetime
 
 class UserItemMatrix:
 
-    def __init__(self, user_info_path, item_info_path):
-        self.dataset = Dataset()
-
-        self._build_user_info_dict(user_info_path)
-        self._build_item_info_dict(item_info_path)
-        
-
-    def _build_user_info_dict(self, file_path):
+    def __init__(self):
+        pass    
+    
+    def build_user_info_matrix(self, file_path):
         data = pd.read_table(file_path, delimiter='|', header=None)
-        
+        n_users = len(data)
+        n_features = 3
+
+        mat = scipy.sparse.lil_matrix((n_users, n_features), dtype=np.int32)
+
         gender_encoder = LabelEncoder()
         gender_encoder.fit(data.iloc[:, 2])
 
         occupation_encoder = LabelEncoder()
         occupation_encoder.fit(data.iloc[:, 3])
-        
         for _, row in data.iterrows():
             user_id, age, gender, occupation, zipcode = row[0], row[1], row[2], row[3], row[4]
             gender = gender_encoder.transform([gender])[0]
             occupation = occupation_encoder.transform([occupation])[0]
 
-            self.dataset.fit_partial(users=[user_id],
-                                     user_features=[(age, gender, occupation)])
+            mat[user_id - 1, :] = [age, gender, occupation]
 
-    def _build_item_info_dict(self, file_path):
+        return mat
+
+    def build_item_info_matrix(self, file_path):
         data = pd.read_table(file_path, delimiter='|', header=None)
+        n_items = len(data)
+        n_features = 1
 
+        mat = scipy.sparse.lil_matrix((n_items, n_features), dtype=np.int32)
         for _, row in data.iterrows():
             item_id, release_str = row[0], row[2]
 
             if type(release_str) is not str:
-                self.dataset.fit_partial(items=[item_id],
-                                         item_features=[0])
+                mat[item_id - 1, :] = [0]
             else:
                 release_date = datetime.datetime.strptime(release_str, '%d-%b-%Y')
                 release_date_int = int(release_date.strftime('%s'))
 
-                self.dataset.fit_partial(items=[item_id],
-                                         item_features=[release_date_int])
-    
-    def build_user_info_matrix(self, file_path):
-        data = pd.read_table(file_path, delimiter='|', header=None)
+                mat[item_id - 1, :] = [release_date_int]
 
-        
-    
-    def load(self, file_path):
+        return mat
+
+    def build_interactions_matrix(self, rows, cols, file_path):
         data = pd.read_table(file_path, delimiter='\t', header=None)
 
-        # update user rating
+        mat = scipy.sparse.lil_matrix((rows, cols), dtype=np.int32)
+
         for _, row in data.iterrows():
             userId, itemId, rating = row[0], row[1], row[2]
+            mat[userId - 1, itemId - 1] = rating
 
-            self.dataset.build_intera
-            is_like = rating > 2
-
-            is_already_like = self.user_rating[userId][itemId - 1] == 1
-            if not is_already_like:
-                self.user_rating[userId][itemId - 1] = is_like
-
-        users = []
-        items = []
-        user_features = []
-        item_features = []
-
-        for _, row in data.iterrows():
-            userId, itemId = row[0], row[1]
-            
-            uf = self.user_info[userId]
-            uf = np.append(uf, self.user_rating[userId])
-            itemf = self.item_info[itemId]
-
-            users.append(userId)
-            items.append(itemId)
-            user_features.append(uf)
-            item_features.append(itemf)
-
-        self.dataset.fit(users, items)
-
+        return mat
 
 # Unit test
 
 if __name__ == '__main__':
-    uimatrix = UserItemMatrix('../data/ml-100k/u.user', '../data/ml-100k/u.item')
-    uimatrix.load('../data/ml-100k/u1.base')
+    uimatrix = UserItemMatrix()
+    user_info = uimatrix.build_user_info_matrix('../data/ml-100k/u.user')
+    item_info = uimatrix.build_item_info_matrix('../data/ml-100k/u.item')
+
+    n_users = user_info.shape[0]
+    n_items = item_info.shape[0]
+    interactions = uimatrix.build_interactions_matrix(n_users, n_items, '../data/ml-100k/u1.base')
+
+    print(interactions)
--- recommender/lightfm/rec.py
+++ recommender/lightfm/rec.py
@@ -1,33 +1,42 @@
 from lightfm import LightFM
 from lightfm.evaluation import precision_at_k
 from lightfm.evaluation import auc_score
-from lightfm.data import Dataset
+
+from matrix_builder import UserItemMatrix 
 
 import pandas as pd
 import numpy as np
 
-item_info_file = None
-uesr_info_file = None
-
-def build_item_info_dict():
-    pass
-
-def build_user_item_matrix():
-    dataset = Dataset()
     
 if __name__ == '__main__':
-    global item_info_file
-    global uesr_info_file
+    
+    mb = UserItemMatrix()
+    
+    user_info = mb.build_user_info_matrix('../data/ml-100k/u.user')
+    item_info = mb.build_item_info_matrix('../data/ml-100k/u.item')
 
-    item_info_file = pd.read_csv('../data/ml-100k/u.item')
-    uesr_info_file = pd.read_csv('../data/ml-100k/u.user')
+    n_users = user_info.shape[0]
+    n_items = item_info.shape[0]
 
-    train = pd.read_csv('../data/ml-100k/ua.base')
-    test = pd.read_csv('../data/ml-100k/ua.test')
+    print(user_info.shape)
+    train = mb.build_interactions_matrix(n_users, n_items, '../data/ml-100k/u1.base')
 
-    model = LightFM(learning_rate=0.05, loss='bpr')
-    model.fit(train, epochs=10)
+    test = mb.build_interactions_matrix(n_users, n_items, '../data/ml-100k/u1.test')
 
+    model = LightFM(learning_rate=0.05, loss='bpr', no_components=30)
+    # model.fit(train, epochs=10)
+  
+    # train_precision = precision_at_k(model, train, k=10).mean()
+    # test_precision = precision_at_k(model, test, k=10).mean()
+
+    # train_auc = auc_score(model, train).mean()
+    # test_auc = auc_score(model, test).mean()
+
+    # print('Precision: train %.2f, test %.2f.' % (train_precision, test_precision))
+    # print('AUC: train %.2f, test %.2f.' % (train_auc, test_auc))
+    # print('=================')
+
+    model.fit(train, user_features=user_info, epochs=10)
     train_precision = precision_at_k(model, train, k=10).mean()
     test_precision = precision_at_k(model, test, k=10).mean()
 
@@ -36,3 +45,4 @@ if __name__ == '__main__':
 
     print('Precision: train %.2f, test %.2f.' % (train_precision, test_precision))
     print('AUC: train %.2f, test %.2f.' % (train_auc, test_auc))
+    print('=================')
